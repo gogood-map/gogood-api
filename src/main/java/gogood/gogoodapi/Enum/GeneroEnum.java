19265011efe4074f4397be89780b024c93a07a7a
lean-dro <leandro.saraiva@sptech.school>
2024-03-07 18:02:46 -0300

    Adição de dependências e conexão inicial com o banco

--- src/main/java/gogood/gogoodapi/Enum/GeneroEnum.java
+++ src/main/java/gogood/gogoodapi/Enum/GeneroEnum.java
@@ -1,4 +1,15 @@
 package gogood.gogoodapi.Enum;
 
 public enum GeneroEnum {
+    MASCULINO("M"),
+    FEMININO("F"),
+    OUTROS("O"),
+    PREFIRO_NAO_DIZER("PFN");
+
+    GeneroEnum(String sigla) {
+    }
+
+    String getGeneroSigla(String sigla){
+        return sigla;
+    }
 }

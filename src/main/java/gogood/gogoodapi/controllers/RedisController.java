5318f3c1b6737fe5c38366d4d150bc87cf8ce488
Thiago <thiago.romaris@sptech.school>
2024-04-03 22:32:46 -0300

    R2DBC

--- src/main/java/gogood/gogoodapi/controllers/RedisController.java
+++ src/main/java/gogood/gogoodapi/controllers/RedisController.java
@@ -2,47 +2,108 @@ package gogood.gogoodapi.controllers;
 
 import gogood.gogoodapi.models.MapList;
 import gogood.gogoodapi.models.redis.config.GenericConverter;
-import gogood.gogoodapi.repository.MapRepository;
-import gogood.gogoodapi.services.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @EnableCaching
 @RequestMapping("/consultar")
 public class RedisController {
 
-    @Autowired
-    RedisService redisService;
+
+    public final List<MapList> partes = new ArrayList<>();
 
     @Autowired
     private ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
 
-    @GetMapping
-    public Mono<ResponseEntity<String>> getDadosOcorrencias() {
-        return Mono.fromCallable(() -> {
-            List<MapList> dados = redisService.get(reactiveRedisTemplate);
-            return ResponseEntity.ok("Ok");
-        }).onErrorReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro"));
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
+
+    public RedisController(R2dbcEntityTemplate r2dbcEntityTemplate) {
+        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
     }
 
-    @GetMapping("/local/{latitude}/{longitude}")
-    public Mono<ResponseEntity<MapList>> location(@PathVariable Double latitude, @PathVariable Double longitude) {
-        return redisService.getLocationCached(latitude, longitude)
-                .map(resultado -> ResponseEntity.ok().body(resultado))
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+    @GetMapping
+    public ResponseEntity<String> getDadosOcorrencias() {
+        get();
+        return ResponseEntity.ok("Ok");
     }
+
+    public Mono<Void> saveListReactive(List<Map<String, Object>> mapData) {
+        int batchSize = 10000;
+        return Flux.fromIterable(mapData)
+                .buffer(batchSize) // Agrupa os mapas em lotes de 10000
+                .index() // Fornece um índice para cada lote
+                .flatMap(tuple -> {
+                    List<Map<String, Object>> batch = tuple.getT2();
+                    String batchId = "lista" + (tuple.getT1() + 1);
+                    // Transforma o lote em um objeto MapList ou similar
+                    MapList mapList = new MapList();
+                    mapList.setMapData(batch);
+                    mapList.setId(batchId);
+                    synchronized (partes) {
+                        partes.add(mapList); // Adiciona à lista de forma sincronizada
+                    }
+                    // Salvar no Redis ou outro armazenamento de forma reativa
+                    return salvarListaNoRedis(partes);
+                })
+                .then(); // Conclui a cadeia retornando um Mono<Void> quando todas as gravações estiverem completas
+    }
+
+
+    public Mono<Void> get() {
+        Flux<Map<String, Object>> mapDataFlux = r2dbcEntityTemplate.getDatabaseClient().sql("SELECT * FROM ocorrencias")
+                .map((row, metadata) -> {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("id", row.get("id", Integer.class));
+                    map.put("latitude", row.get("latitude", Double.class));
+                    map.put("longitude", row.get("longitude", Double.class));
+                    return map;
+                }).all();
+
+        return mapDataFlux.collectList()
+                .flatMap(this::saveListReactive)
+                .doOnError(e -> System.out.println("Erro durante a execução: " + e.getMessage()));
+
+    }
+
+
+    public Mono<Void> salvarListaNoRedis(List<MapList> partes) {
+        return Flux.fromIterable(partes)
+                .flatMap(item -> {
+                    String chave = "parte:" + item.getId();
+                    return reactiveRedisTemplate.opsForValue().set(chave, item);
+                })
+                .then();
+    }
+
+//    @GetMapping("/local/{latitude}/{longitude}")
+//    public Mono<ResponseEntity<MapList>> location(@PathVariable Double latitude, @PathVariable Double longitude) {
+//        return getLocationCached(latitude, longitude)
+//                .map(resultado -> ResponseEntity.ok().body(resultado))
+//                .defaultIfEmpty(ResponseEntity.notFound().build());
+//    }
+//
+//    @Cacheable(value = "localizacao", key = "#latitude.toString().concat('-').concat(#longitude.toString())")
+//    @Transactional(readOnly = true)
+//    public Mono<MapList> getLocationCached(Double latitude, Double longitude) {
+//        return Mono.fromCallable(() -> getByLocation(latitude, longitude));
+//    }
 
 //    @GetMapping("/{id}")
 //    public ResponseEntity<MapList> resultadoRedis(@PathVariable String id) {
--- src/main/java/gogood/gogoodapi/services/RedisService.java
+++ src/main/java/gogood/gogoodapi/services/RedisService.java
@@ -1,121 +1,68 @@
-package gogood.gogoodapi.services;
-
-import gogood.gogoodapi.models.MapData;
-import gogood.gogoodapi.models.MapList;
-import gogood.gogoodapi.models.Ocorrencias;
-import gogood.gogoodapi.models.config.JdbcConfig;
-import gogood.gogoodapi.models.redis.config.GenericConverter;
-import gogood.gogoodapi.repository.GoGoodRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Mono;
-
-import java.util.*;
-
-@Service
-public class RedisService {
-    JdbcConfig jdbcConfig = new JdbcConfig();
-    List<MapList> partes = new ArrayList<>();
-
-    private ReactiveRedisTemplate redisTemplate;
-
-    @Cacheable(value = "localizacao", key = "#latitude.toString().concat('-').concat(#longitude.toString())")
-    @Transactional(readOnly = true)
-    public Mono<MapList> getLocationCached(Double latitude, Double longitude) {
-        return Mono.fromCallable(() -> getByLocation(latitude, longitude));
-    }
-
-    public void salvarListaNoRedis() {
-        for (MapList item : partes) {
-            String chave = "parte:" + item.getId();
-            redisTemplate.opsForValue().set(chave, item);
-        }
-    }
-    public List<MapList> get(ReactiveRedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-        List<MapData> resultado = jdbcConfig.getConexaoDoBanco().query("""
-                SELECT * FROM ocorrencias
-                 """, new BeanPropertyRowMapper<>(MapData.class));
-
-        MapList mapList = new MapList();
-        List<Map<String, Object>> mapData = new ArrayList<>();
-
-        for (MapData mapa : resultado) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("longitude", mapa.getLongitude());
-            map.put("latitude", mapa.getLatitude());
-            map.put("id", mapa.getId());
-            mapData.add(map);
-        }
-
-        saveList(mapData);
-        return partes;
-    }
-
-    public MapList getByLocation(Double latitude, Double longitude) {
-        List<MapData> resultado = jdbcConfig.getConexaoDoBanco().query("""
-                SELECT *, (6371 * acos(
-                        cos(radians(?)) * cos(radians(LATITUDE)) * cos(radians(LONGITUDE) - radians(?)) +
-                        sin(radians(?)) * sin(radians(LATITUDE))
-                    )) AS distancia
-                FROM ocorrencias
-                HAVING distancia <= 2;
-                """, new Object[]{latitude, longitude, latitude}, new BeanPropertyRowMapper<>(MapData.class));
-
-        MapList mapList = new MapList();
-        List<Map<String, Object>> mapData = new ArrayList<>();
-
-        for (MapData mapa : resultado) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("longitude", mapa.getLongitude());
-            map.put("latitude", mapa.getLatitude());
-            map.put("id", mapa.getId());
-            mapData.add(map);
-        }
-
-        mapList.setMapData(mapData);
-        mapList.setId("listaLocalizacao");
-//        mapRepository.save(mapList);
-
-        return mapList;
-    }
-
-    public void saveList(List<Map<String, Object>> mapData){
-        int totalPartes = (mapData.size() + 9999) / 10000;
-
-        for (int parte = 0; parte < totalPartes; parte++) {
-            int start = parte * 10000;
-            int end = Math.min(start + 10000, mapData.size());
-
-            List<Map<String, Object>> subList = mapData.subList(start, end);
-
-            MapList mapList = new MapList();
-            mapList.setMapData(subList);
-            mapList.setId("lista" + (parte + 1));
-            partes.add(mapList);
-        }
-
-        salvarListaNoRedis();
-    }
-
-//    public Ocorrencias getById(Integer id, GoGoodRepository goGoodRepository) {
-//        Optional<Ocorrencias> resultado = goGoodRepository.findById(id);
-//        if (resultado.isPresent()) {
-//            return GenericConverter.convert(resultado.get(), Ocorrencias.class);
-//        } else {
-//            throw new RuntimeException("Não existe lista procurada no Redis no ID: " + id);
-//        }
-//    }
-//    @Cacheable(value = "listaEmCache", key = "#id")
-//    @Transactional(readOnly = true)
-//    public Ocorrencias getCachedMapList(Integer id, GoGoodRepository goGoodRepository) {
-//        return getById(id, goGoodRepository);
-//    }
-}
+//package gogood.gogoodapi.services;
+//
+//import gogood.gogoodapi.models.MapData;
+//import gogood.gogoodapi.models.MapList;
+//import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.data.redis.core.ReactiveRedisTemplate;
+//import org.springframework.jdbc.core.BeanPropertyRowMapper;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//import reactor.core.publisher.Mono;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//@Service
+//public class RedisService {
+//
+//
+//    private ReactiveRedisTemplate redisTemplate;
+//
+//
+//
+////    public MapList getByLocation(Double latitude, Double longitude) {
+////        List<MapData> resultado = jdbcConfig.getConexaoDoBanco().query("""
+////                SELECT *, (6371 * acos(
+////                        cos(radians(?)) * cos(radians(LATITUDE)) * cos(radians(LONGITUDE) - radians(?)) +
+////                        sin(radians(?)) * sin(radians(LATITUDE))
+////                    )) AS distancia
+////                FROM ocorrencias
+////                HAVING distancia <= 2;
+////                """, new Object[]{latitude, longitude, latitude}, new BeanPropertyRowMapper<>(MapData.class));
+////
+////        MapList mapList = new MapList();
+////        List<Map<String, Object>> mapData = new ArrayList<>();
+////
+////        for (MapData mapa : resultado) {
+////            Map<String, Object> map = new HashMap<>();
+////            map.put("longitude", mapa.getLongitude());
+////            map.put("latitude", mapa.getLatitude());
+////            map.put("id", mapa.getId());
+////            mapData.add(map);
+////        }
+////
+////        mapList.setMapData(mapData);
+////        mapList.setId("listaLocalizacao");
+//////        mapRepository.save(mapList);
+////
+////        return mapList;
+////    }
+//
+//
+//
+////    public Ocorrencias getById(Integer id, GoGoodRepository goGoodRepository) {
+////        Optional<Ocorrencias> resultado = goGoodRepository.findById(id);
+////        if (resultado.isPresent()) {
+////            return GenericConverter.convert(resultado.get(), Ocorrencias.class);
+////        } else {
+////            throw new RuntimeException("Não existe lista procurada no Redis no ID: " + id);
+////        }
+////    }
+////    @Cacheable(value = "listaEmCache", key = "#id")
+////    @Transactional(readOnly = true)
+////    public Ocorrencias getCachedMapList(Integer id, GoGoodRepository goGoodRepository) {
+////        return getById(id, goGoodRepository);
+////    }
+//}

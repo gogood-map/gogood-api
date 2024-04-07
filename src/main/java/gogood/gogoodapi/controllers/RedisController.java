26d5c76f0d55f759755f2ebdc8c6edf7c2aaea28
Thiago Garcia Romaris <thiago.romaris@sptech.school>
2024-04-07 16:11:49 -0300

    WebFlux v2

--- src/main/java/gogood/gogoodapi/controllers/RedisController.java
+++ src/main/java/gogood/gogoodapi/controllers/RedisController.java
@@ -1,33 +1,34 @@
 package gogood.gogoodapi.controllers;
 
-import gogood.gogoodapi.models.MapData;
 import gogood.gogoodapi.models.MapList;
-import gogood.gogoodapi.models.config.JdbcConfig;
 import gogood.gogoodapi.models.redis.config.GenericConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @EnableCaching
+@EnableScheduling
 @RequestMapping("/consultar")
 public class RedisController {
 
-    public final List<MapList> partes = Collections.synchronizedList(new ArrayList<>());
-
-    private final AtomicInteger counter = new AtomicInteger(1);
+    public final List<MapList> partes = new ArrayList<>();
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
@@ -35,18 +36,17 @@ public class RedisController {
     @Autowired
     private ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
 
-    JdbcConfig jdbcConfig = new JdbcConfig();
-//
-//    private final R2dbcEntityTemplate r2dbcEntityTemplate;
-//
-//    public RedisController(R2dbcEntityTemplate r2dbcEntityTemplate) {
-//        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
-//    }
+//    JdbcConfig jdbcConfig = new JdbcConfig();
 
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
+
+    public RedisController(R2dbcEntityTemplate r2dbcEntityTemplate) {
+        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
+    }
     @GetMapping
     public Mono<ResponseEntity<String>> getDadosOcorrencias() {
-        get();
-        return Mono.just(ResponseEntity.ok().body("Dados salvos com sucesso"));
+        return get()
+                .then(Mono.just(ResponseEntity.ok().body("Dados salvos com sucesso")));
     }
 
 
@@ -56,36 +56,24 @@ public class RedisController {
         for (int parte = 0; parte < totalPartes; parte++) {
             int start = parte * 10000;
             int end = Math.min(start + 10000, mapData.size());
-
             List<Map<String, Object>> subList = mapData.subList(start, end);
-
             MapList mapList = new MapList();
             mapList.setMapData(subList);
             mapList.setId("lista" + (parte + 1));
             partes.add(mapList);
         }
-
         salvarListaNoRedis();
     }
 
-
-    public void get() {
-        List<MapData> resultado = jdbcConfig.getConexaoDoBanco().query("""
-                SELECT * FROM ocorrencias
-                 """, new BeanPropertyRowMapper<>(MapData.class));
-
-        MapList mapList = new MapList();
-        List<Map<String, Object>> mapData = new ArrayList<>();
-
-        for (MapData mapa : resultado) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("longitude", mapa.getLongitude());
-            map.put("latitude", mapa.getLatitude());
-            map.put("id", mapa.getId());
-            mapData.add(map);
-        }
-
-        saveList(mapData);
+    @Scheduled(fixedRate = 30000)
+    public Mono<Void> get() {
+        String query = "SELECT * FROM ocorrencias";
+        return r2dbcEntityTemplate.getDatabaseClient().sql(query)
+                .fetch()
+                .all()
+                .collectList()
+                .doOnNext(this::saveList)
+                .then();
     }
 
 
@@ -104,7 +92,6 @@ public class RedisController {
 //    }
 //
 //    @Cacheable(value = "localizacao", key = "#latitude.toString().concat('-').concat(#longitude.toString())")
-//    @Transactional(readOnly = true)
 //    public Mono<MapList> getLocationCached(Double latitude, Double longitude) {
 //        return Mono.fromCallable(() -> getByLocation(latitude, longitude));
 //    }
@@ -112,7 +99,7 @@ public class RedisController {
 //    @GetMapping("/{id}")
 //    public ResponseEntity<MapList> resultadoRedis(@PathVariable String id) {
 //        try {
-//            MapList resultado = redisService.getCachedMapList(id, mapRepository);
+//            MapList resultado = getCachedMapList(id, mapRepository);
 //            return ResponseEntity.ok().body(resultado);
 //        } catch (RuntimeException e) {
 //            return ResponseEntity.notFound().build();

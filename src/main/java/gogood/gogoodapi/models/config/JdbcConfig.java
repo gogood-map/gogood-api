989123bd906cf29db7323970efa6b43e69e1d7fc
Thiago Garcia Romaris <thiago.romaris@sptech.school>
2024-04-14 22:09:26 -0300

    Aplicação estável

---
+++ src/main/java/gogood/gogoodapi/models/config/JdbcConfig.java
@@ -0,0 +1,18 @@
+package gogood.gogoodapi.models.config;
+import lombok.Getter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+@Getter
+public class JdbcConfig {
+    private JdbcTemplate conexaoDoBanco;
+
+    public JdbcConfig() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://52.226.122.160:3306/GoGood");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Root@123");
+        conexaoDoBanco = new JdbcTemplate(dataSource);
+    }
+}

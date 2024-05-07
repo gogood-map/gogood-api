f1eb664276b495b1d0bfec60a0b511d8a85d0eac
Thiago <thiago.romaris@sptech.school>
2024-05-07 17:17:12 -0300

    Alterando configurações estéticas do Swagger

--- src/main/java/gogood/gogoodapi/controllers/RotasController.java
+++ src/main/java/gogood/gogoodapi/controllers/RotasController.java
@@ -8,6 +8,7 @@ import gogood.gogoodapi.domain.strategy.rotaStrategy.BicicletaStrategy;
 import gogood.gogoodapi.domain.strategy.rotaStrategy.TransportePublicoStrategy;
 import gogood.gogoodapi.domain.strategy.rotaStrategy.VeiculoStrategy;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/rotas")
+@Tag(name = "Rotas", description = "Consultar rotas de transporte")
 public class RotasController {
 
     @Autowired

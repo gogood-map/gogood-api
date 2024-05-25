99edaa859a5875865ef72b60348ede756883a2e8
Thiago <thiago.romaris@sptech.school>
2024-05-25 16:54:44 -0300

    Redis health chech

--- src/main/java/gogood/gogoodapi/configuration/redis/RedisConfiguration.java
+++ src/main/java/gogood/gogoodapi/configuration/redis/RedisConfiguration.java
@@ -3,10 +3,19 @@ package gogood.gogoodapi.configuration.redis;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gogood.gogoodapi.configuration.redis.config.RedisByteToMapListConverter;
 import gogood.gogoodapi.configuration.redis.config.RedisMapListToBytesConverter;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.convert.RedisCustomConversions;
@@ -19,6 +28,45 @@ import java.util.Arrays;
 
 @Configuration
 public class RedisConfiguration {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis..port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis..password}")
+    private String redisPassword;
+
+    @Bean(destroyMethod = "shutdown")
+    public ClientResources clientResources() {
+        return DefaultClientResources.create();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(ClientResources clientResources) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        config.setPassword(redisPassword);
+
+        GenericObjectPoolConfig<Object> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxTotal(8);
+        poolConfig.setMaxIdle(8);
+        poolConfig.setMinIdle(0);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setTimeBetweenEvictionRuns(Duration.ofDays(Duration.ofMinutes(1).toMillis())); // Verifica conexões a cada minuto
+        poolConfig.setMinEvictableIdleTime(Duration.ofDays(Duration.ofMinutes(1).toMillis())); // Conexões ociosas por mais de 5 minutos serão fechadas
+
+        LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(10))
+                .shutdownTimeout(Duration.ofSeconds(30))
+                .clientResources(clientResources)
+                .poolConfig(poolConfig)
+                .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
+    }
+
 
     @Bean
     public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
@@ -41,10 +89,11 @@ public class RedisConfiguration {
         ));
     }
 
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, RedisCustomConversions redisCustomConversions) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        template.setConnectionFactory(redisConnectionFactory(clientResources()));
         template.setKeySerializer(new StringRedisSerializer());
 
         Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);

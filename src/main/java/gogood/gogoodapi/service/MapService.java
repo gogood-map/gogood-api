978f99c8c4879464e0bb60f4a6a1f2025ce9e8ef
Thiago <thiago.romaris@sptech.school>
2024-11-28 19:03:09 -0300

    Novo endpoint e config LatLng

--- src/main/java/gogood/gogoodapi/service/MapService.java
+++ src/main/java/gogood/gogoodapi/service/MapService.java
@@ -1,5 +1,6 @@
 package gogood.gogoodapi.service;
 
+import com.google.maps.model.LatLng;
 import gogood.gogoodapi.domain.models.MapData;
 import gogood.gogoodapi.domain.models.Ocorrencia;
 import gogood.gogoodapi.repository.MapRepository;
@@ -62,7 +63,6 @@ public class MapService {
 
         response.put("top5Ocorrencias", top5Ocorrencias);
         response.put("mesOcorrencias", mes);
-
         return response;
     }
 
@@ -123,4 +123,17 @@ public class MapService {
         return crimeData;
     }
 
+    public List<LatLng> searchRouteOcorrenciasMobile(Double latitude, Double longitude, Double raio) {
+        Point localizacao = new Point(longitude, latitude);
+        Distance distancia = new Distance(raio, Metrics.KILOMETERS);
+        List<Ocorrencia> ocorrencias = mapRepository.findByLocalizacaoNear(localizacao, distancia);
+        return ocorrencias.stream()
+                .map(ocorrencia -> new LatLng(
+                        ocorrencia.getLocalizacao().getY(),
+                        ocorrencia.getLocalizacao().getX()
+                ))
+                .collect(Collectors.toList());
+
+
+    }
 }

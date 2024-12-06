4fc7a44412917d5d17a43bbca9a94082163db93b
Thiago <thiago.romaris@sptech.school>
2024-12-06 18:33:37 -0300

    Colocado log info pra teste

--- src/main/java/gogood/gogoodapi/service/ClientGoogleMaps.java
+++ src/main/java/gogood/gogoodapi/service/ClientGoogleMaps.java
@@ -5,10 +5,12 @@ import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.TravelMode;
 import com.google.maps.model.Unit;
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 public class ClientGoogleMaps {
     public static GeoApiContext gerarContextoClient(){
         Dotenv dotenv = Dotenv.load();
@@ -29,6 +31,7 @@ public class ClientGoogleMaps {
         return request;
     }
     public static DirectionsResult obterRespostaRota(String origem, String destino, TravelMode modoTransporte) {
+        log.info("Obtendo resposta da rota (obterRespostaRota)");
         DirectionsApiRequest request = gerarRequestRota();
         request.mode(modoTransporte);
         request.destination(destino);
@@ -36,6 +39,7 @@ public class ClientGoogleMaps {
         try {
             return request.await();
         } catch (Exception e) {
+            log.error("Erro ao obter resposta da rota (obterRespostaRota)");
             throw new ResponseStatusException(HttpStatusCode.valueOf(500), "Não foi possível gerar rota. Detalhe mais a origem/destino ou tente novamente mais tarde");
         }
     }
